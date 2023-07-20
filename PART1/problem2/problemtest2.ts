80f8109094971723233f7bb5109fae3f6a63e01b
ZainulAltera <mzainullah0798@gmail.com>
2023-07-20 18:18:29 +0700

    ppronlemtest2

---
+++ PART1/problem2/problemtest2.ts
@@ -0,0 +1,35 @@
+function dragonOfLoowater(dragonHead: number[], knightHeight: number[]) {
+  let heightSum = 0;
+
+  let newDragon = dragonHead.sort((a, b) => a - b);
+  let newKnight = knightHeight.sort((a, b) => a - b);
+
+  if (newKnight.length <= newDragon.length) {
+    console.log("knight fall");
+    return;
+  }
+
+  for (let i = 0; i < newDragon.length; i++) {
+    for (let j = 0; j < newKnight.length; j++) {
+      if (newDragon[i] <= newKnight[j]) {
+        heightSum += newKnight[j];
+        break;
+      }
+      if (j == newKnight.length - 1) {
+        console.log("knight fall");
+        return;
+      }
+    }
+  }
+  if (heightSum == 0) {
+    console.log(heightSum);
+    return;
+  }
+  console.log(heightSum);
+  return;
+}
+
+dragonOfLoowater([5, 4], [7, 8, 4]); //11
+dragonOfLoowater([15, 10], [5]); //knight fall
+dragonOfLoowater([7, 2], [4, 3, 1, 2]); //knight fall
+dragonOfLoowater([7, 2], [2, 1, 8, 5]); //10
