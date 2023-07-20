856d5274791bf84d54f0c1032e4b188bacad724b
ZainulAltera <mzainullah0798@gmail.com>
2023-07-20 08:40:48 +0700

    problem2.ts

--- PART1/problem2.ts
+++ PART1/problem2.ts
@@ -1,29 +1,22 @@
-function atbashCipher(text: string): string {
-  const plainAlphabet = "abcdefghijklmnopqrstuvwxyz";
-  const cipherAlphabet = "zyxwvutsrqponmlkjihgfedcba";
-
-  // Convert the input text to lowercase for consistency
-  const lowercaseText = text.toLowerCase();
+function decrytAtbash(text: string): string {
+  const teks = "abcdefghijklmnopqrstuvwxyz";
+  const teks1 = "zyxwvutsrqponmlkjihgfedcba";
+  const teks3 = text.toLowerCase();
 
   let result = "";
-  for (let i = 0; i < lowercaseText.length; i++) {
-    const char = lowercaseText[i];
-    const index = plainAlphabet.indexOf(char);
-
-    // If the character is not present in the plain alphabet (e.g., special characters, numbers),
-    // simply append it to the result as it is.
+  for (let i = 0; i < teks3.length; i++) {
+    const char = teks3[i];
+    const index = teks.indexOf(char);
     if (index === -1) {
       result += char;
     } else {
-      // Transpose the letter using the cipher alphabet
-      result += cipherAlphabet[index];
+      result += teks1[index];
     }
   }
 
   return result;
 }
 
-// Example usage:
-const inputText = "Olivn rkhfn wloli hrg znvg";
-const encryptedText = atbashCipher(inputText);
-console.log(encryptedText); // Output: "Svool, Dliow!"
+console.log(decrytAtbash("Olivn rkhfn wloli hrg znvg")); //lorem ipsum dolor sit amet
+console.log(decrytAtbash("Svool dliow! R'n ovzimrmt gbkvhxirkg!")); //hello world! i'm learning typescript!
+console.log(decrytAtbash("Gsv jfrxp yildm ulc qfnkh levi gsv ozab wlt")); //the quick brown fox jumps over the lazy dog
